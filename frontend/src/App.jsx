f959fb0d79e3162a9881386bd0ee1b483fe42f39
root <root@DESKTOP-V7H5TLA>
2023-06-24 01:31:04 +0200

    add UI Elements

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import SignupPage from "./pages/signup-page/signup-page";
+
+function App() {
+  return (
+    <div className="app">
+      <SignupPage />
+    </div>
+  );
+}
+
+export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./assets/fonts/Inter-ExtraBold.ttf";
+import "./assets/fonts/Inter-ExtraLight.ttf";
+import "./assets/fonts/Inter-Light.ttf";
+import "./assets/fonts/Inter-Medium.ttf";
+import "./assets/fonts/Inter-Regular.ttf";
+import "./assets/fonts/Inter-SemiBold.ttf";
+import "./assets/fonts/Inter-Thin.ttf";
 import "./index.css";
 import App from "./App";
 
