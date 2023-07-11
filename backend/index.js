069ae208f7d236b80cdb4b02d17b6e7bd8b00751
nschaef1 <nils.schaefer@htwk.stud-leipzig.de>
2023-07-11 13:50:05 +0200

    add websession token

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,19 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const jsonWebToken = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 const User = require("./models/user");
 
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL);
 const jsonWebTokenSecret = process.env.JWT_SECRET;
+const bcryptSalt = bcrypt.genSaltSync(10);
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
@@ -18,19 +22,60 @@ app.use(
   })
 );
 
+app.get("/profile", async (req, res) => {
+  const token = req.cookies?.token;
+  if (token) {
+    jsonWebToken.verify(token, jsonWebTokenSecret, {}, (err, userData) => {
+      if (err) throw err;
+      res.json(userData);
+    });
+  } else {
+    res.status(401).json("no token");
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const foundUser = await User.findOne({ username });
+  if (foundUser) {
+    const valid = bcrypt.compareSync(password, foundUser.password);
+    if (valid) {
+      jsonWebToken.sign(
+        { userId: foundUser._id, username },
+        jsonWebTokenSecret,
+        {},
+        (err, token) => {
+          res.cookie("token", token).json({
+            id: foundUser._id,
+          });
+        }
+      );
+    }
+  }
+});
+
 app.post("/signup", async (req, res) => {
   const { email, username, password } = req.body;
   try {
-    const createdUser = await User.create({ email, username, password });
+    const hashedPassword = bcrypt.hashSync(password, bcryptSalt);
+    const createdUser = await User.create({
+      email: email,
+      username: username,
+      password: hashedPassword,
+    });
     jsonWebToken.sign(
-      { userId: createdUser._id },
+      { userId: createdUser._id, username },
       jsonWebTokenSecret,
       {},
       (err, token) => {
         if (err) throw err;
-        res.cookie("token", token).status(201).json({
-          id: createdUser._id,
-        });
+        res
+          .cookie("token", token, { sameSite: "none", secure: true })
+          .status(201)
+          .json({
+            id: createdUser._id,
+            username,
+          });
       }
     );
   } catch (err) {
