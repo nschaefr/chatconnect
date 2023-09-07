7dd88cb0c50399d9d9ec24383dcd3789a6e2b8b0
tpitzing <tim.pitzing@stud.htwk.leipzig.de>
2023-09-07 18:28:00 +0200

    Adjust styling from docs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,7 @@
 import axios from "axios";
 import { ContextProvider } from "./components/utils/user-context";
 import Routes from "./Routes.jsx";
-/**
- * Landing Page
- */
+
 function App() {
   axios.defaults.baseURL = "http://localhost:4040";
   axios.defaults.withCredentials = true;
