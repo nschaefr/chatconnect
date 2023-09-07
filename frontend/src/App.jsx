f9cd3515fd409ef4ac4a81af4d5bd3c59c75df0f
tpitzing <tim.pitzing@stud.htwk.leipzig.de>
2023-09-07 17:12:45 +0200

    Added documentation to the frontend run with npm rund docs and then look in the docs Folder in the index.html

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
 import axios from "axios";
 import { ContextProvider } from "./components/utils/user-context";
 import Routes from "./Routes.jsx";
-
+/**
+ * Landing Page
+ */
 function App() {
   axios.defaults.baseURL = "http://localhost:4040";
   axios.defaults.withCredentials = true;
