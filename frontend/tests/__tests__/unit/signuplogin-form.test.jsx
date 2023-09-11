c7f73af3fc58ac157b2ecdb930efd74f2376c608
nschaef1 <nils.schaefer@stud.htwk-leipzig.de>
2023-09-11 11:13:10 +0200

    add tests

--- frontend/tests/__tests__/unit/signuplogin-form.test.jsx
+++ frontend/tests/__tests__/unit/signuplogin-form.test.jsx
@@ -1,6 +1,11 @@
 import React from "react";
-import { render, fireEvent, screen } from "@testing-library/react";
+import { render, fireEvent, screen, waitFor } from "@testing-library/react";
 import SignuploginForm from "../../../src/components/signuplogin-page/signuplogin-form";
+const axios = require("axios");
+import jwt from "jsonwebtoken";
+
+jest.mock("axios");
+jest.mock("jsonwebtoken");
 
 describe("rendering error messages on loginform", () => {
   beforeEach(() => {
@@ -35,7 +40,11 @@ describe("rendering error messages on loginform", () => {
     expect(invalidDataError).toBeInTheDocument();
   });
 
-  it("invalid-data-error visible when data invalid because of invalid username", () => {
+  it("invalid-data-error visible when data invalid because of invalid username", async () => {
+    jest.spyOn(axios, "post").mockImplementation(() => {
+      return Promise.resolve({ data: { valid: false } });
+    });
+
     fireEvent.change(screen.getByTestId("username-input"), {
       target: { value: "invalid-name" },
     });
@@ -43,16 +52,35 @@ describe("rendering error messages on loginform", () => {
       target: { value: "testpassword" },
     });
     fireEvent.click(screen.getByTestId("submit-button"));
+
+    await waitFor(() => {
+      const invalidDataError = screen.getByTestId("invalid-data-error");
+      expect(invalidDataError).toBeInTheDocument();
+    });
   });
 
-  it("invalid-data-error visible when data invalid because of invalid password", () => {
+  it("invalid-data-error visible when data invalid because of invalid password", async () => {
+    jest.spyOn(axios, "post").mockImplementation(() => {
+      return Promise.resolve({ data: { valid: false } });
+    });
+
     fireEvent.change(screen.getByTestId("username-input"), {
       target: { value: "testuser" },
     });
     fireEvent.change(screen.getByTestId("password-input"), {
       target: { value: "invalid-password" },
     });
-    fireEvent.click(screen.getByTestId("submit-button"));
+
+    const passwordInput = screen.getByTestId("password-input");
+    fireEvent.keyDown(passwordInput, {
+      key: "Enter",
+      code: "Enter",
+    });
+
+    await waitFor(() => {
+      const invalidDataError = screen.getByTestId("invalid-data-error");
+      expect(invalidDataError).toBeInTheDocument();
+    });
   });
 });
 
@@ -92,5 +120,66 @@ describe("rendering error messages on signupform", () => {
 
     const canNotBeEmptyError = screen.getByTestId("cannot-be-empty-error");
     expect(canNotBeEmptyError).toBeInTheDocument();
+  });
+
+  it("already-exists-error visible when username is already in use", async () => {
+    jest.spyOn(axios, "post").mockImplementation(() => {
+      return Promise.resolve({ data: { duplicate: true } });
+    });
+
+    fireEvent.change(screen.getByTestId("username-input"), {
+      target: { value: "testuser" },
+    });
+    fireEvent.change(screen.getByTestId("password-input"), {
+      target: { value: "valid-password" },
+    });
+
+    const passwordInput = screen.getByTestId("password-input");
+    fireEvent.keyDown(passwordInput, {
+      key: "Enter",
+      code: "Enter",
+    });
+
+    await waitFor(() => {
+      const alreadyExistsError = screen.getByTestId("already-exists-error");
+      expect(alreadyExistsError).toBeInTheDocument();
+    });
+  });
+});
+
+describe("rendering chat component after valid data", () => {
+  beforeEach(() => {
+    render(<SignuploginForm />);
+  });
+
+  it("successful rendering after login", async () => {
+    const token = jwt.sign(
+      { userId: "mock_id_123", username: "testuser" },
+      "jsonWebTokenSecret",
+      {}
+    );
+    jest.spyOn(axios, "post").mockImplementation(() => {
+      return Promise.resolve({ data: { id: "mock_id_123", valid: true } });
+    });
+
+    jwt.sign.mockReturnValue(token);
+
+    fireEvent.change(screen.getByTestId("username-input"), {
+      target: { value: "testuser" },
+    });
+    fireEvent.change(screen.getByTestId("password-input"), {
+      target: { value: "testpassword" },
+    });
+
+    const passwordInput = screen.getByTestId("password-input");
+    fireEvent.keyDown(passwordInput, {
+      key: "Enter",
+      code: "Enter",
+    });
+
+    await waitFor(() => {
+      const chatComponent = screen.getByTestId("chat-component");
+      expect(chatComponent).toBeInTheDocument();
+    });
   });
 });
